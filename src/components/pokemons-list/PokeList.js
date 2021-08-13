4c709d7bdc4ba79d32ff185a63cf6c33f90c8cdc
Michael Alejandro Rios Rodriguez <71363790+MCLRDRI15@users.noreply.github.com>
2021-08-13 09:36:17 -0500

    ViewMode name solve

--- src/components/pokemons-list/PokeList.js
+++ src/components/pokemons-list/PokeList.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { fetchRequest, handlerFetch } from "../../redux/actions/PokemonActions";
 import PokemonCard from "../pokemons-cards/PokeCards";
-import ViewMode from "../pokemons-selected/viewMode";
+import ViewMode from "../pokemon-selected/ViewMode";
 import "./Pokelist.module.css";
 
 const pokelist = ({ pokemonsList, fetchRequest, handlerFetch, counter }) => {
