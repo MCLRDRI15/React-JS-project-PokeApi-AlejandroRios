607c4dee22fc144d484f1455b7156ae5b38d1ae3
Michael Alejandro Rios Rodriguez <71363790+MCLRDRI15@users.noreply.github.com>
2021-09-15 15:16:08 -0500

    Revert "Revert "Tailwind Styles in the project""

--- src/components/pokemon-selected/ShowPokemonData.js
+++ src/components/pokemon-selected/ShowPokemonData.js
@@ -2,7 +2,6 @@ import React from "react";
 import Charts from "../charts-show-folder/Charts";
 import {FaSkullCrossbones } from "react-icons/fa";
 import ScrollLock from 'react-scrolllock';
-import "./PokemonSelected.module.css";
 
 const ShowPokemonData = ({
   keepPokemon,
@@ -11,10 +10,10 @@ const ShowPokemonData = ({
   cleanPokemonArray,
 }) => {
   return (
-    <div className={singleView ? "h-full fixed top-0 z-40 bg-grey-greyColor overflow-auto cursor-pointer w-screen" : "hidden"}>
+    <div className={singleView ? "h-full fixed top-0 z-40 bg-grey-greyColor overflow-auto cursor-pointer w-screen " : "hidden"}>
       <ScrollLock isActive={singleView}> 
-      <div className="p-4 overflow-auto cursor-auto border-2 border border-gray-900 rounded-xl w-4/12 mt-4 mb-16 mr-auto ml-auto bg-yellow-yellowInput">
-        <div className="p-2 w-full relative border-b border-gray-900 rounded-xl">
+      <div className="w-11/12 laptop:w-4/12 mobile:w-7/12 tablet:w-6/12  p-4 overflow-auto cursor-auto border-2 border-gray-900 rounded-xl mt-4 mb-16 mr-auto ml-auto bg-yellow-yellowInput ">
+        <div className="p-2 w-full relative border-b border-gray-900">
           {pokemonInPokeball.map((pokemon, index = 1) => {
             return (
               <div key={index + Math.random()} className="w-full flex content-between items-center font-semibold text-lg">
@@ -47,23 +46,23 @@ const ShowPokemonData = ({
                       alt="pokemon"
                     />
                   </div>
-                  <div className="w-full">
+                  <div className="w-full flex flex-col text-center">
                     <div className="w-9/12 m-auto text-center font-bold">
                       {pokemon.flavor_text_entries[1].flavor_text}
                     </div>
-                    <div className="w-8/12 text-center m-auto mt-12 mb-4 font-extrabold text-red-500">
+                    <div className=" w-8/12 ml-4 mt-4 text-center mobile:m-auto  mobile:mt-12  mobile:mb-4 font-extrabold text-red-500">
                       <span className="ml-8">Height</span>
                       <span className="ml-8">Weight</span>
                       <span className="ml-8">Gender</span>
                     </div>
-                    <div className="w-8/12 text-center m-auto mb-8 font-medium">
-                      <span className="ml-8">{pokemon.height/10}m</span>
-                      <span className="ml-8">{pokemon.weight/10}kg</span>
-                      <span className="ml-8">{pokemonInPokeball[0].gender}</span>
+                    <div className="w-8/12 text-center ml-8  mobile:m-auto mb-8 font-medium">
+                      <span className="ml-10 text-center mobile:ml-8 ">{pokemon.height/10}m</span>
+                      <span className="ml-11 text-center mobile:ml-8 ">{pokemon.weight/10}kg</span>
+                      <span className="ml-12 text-center mobile:ml-8 ">{pokemonInPokeball[0].gender}</span>
                     </div>
-                    <div className="w-8/12 m-auto mt-4 mb-4 flex content-between items-baseline">
+                    <div className="relative right-12 tablet:left-0 laptop:left-32 w-8/12 text-center m-auto mt-4 mb-4 flex items-center content-between items-baseline">
                       <div className="ml-8 mr-20">
-                        <span className="box">Habilities</span>
+                        <span className="box font-extrabold">Habilities</span>
 
                         <div className="each-ability">
                           <ul>
@@ -78,7 +77,7 @@ const ShowPokemonData = ({
                         </div>
                       </div>
                       <div className="types">
-                        <span className="box">Types</span>
+                        <span className="box font-extrabold">Types</span>
 
                         <div className="each-type">
                           {pokemon.types.map((types) => {
@@ -90,8 +89,8 @@ const ShowPokemonData = ({
                       </div>
                     </div>
                   </div>
-                  <div className="stats">
-                    <span className="box">Stats:</span>
+                  <div className="stats flex flex-col">
+                    <span className="text-center font-extrabold text-xl text-red-500">Stats:</span>
                     <Charts
                       stats={pokemon.stats.map((stat) => {
                         return stat.stat.name;
